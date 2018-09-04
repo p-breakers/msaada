72a0d3dbd1c206900b9d71c907a7d81aeb6936c8
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-04 21:49:25 +0200

    [UPDATE] init.php

--- config/init.php
+++ config/init.php
@@ -5,10 +5,10 @@
  * Date: 04/09/2018
  * Time: 20:49 PM
  */
-
+$Config = array();
 $Config["BASE_URL"] = 'http://'.$_SERVER["SERVER_NAME"]."/banque/";
 $Config["URL_SUFFIX"] = ".html";
 $Config["APP_NAME"] = "Banque";
-$Config["AUTHOR"];
-$Config["MAIL"];
-$Config["PHONE"];
+$Config["AUTHOR"] = "";
+$Config["MAIL"] = "";
+$Config["PHONE"] = "";
