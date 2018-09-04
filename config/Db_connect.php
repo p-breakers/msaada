6d61c7ab6ee5a4ebefeca599bb46ad22b09f9146
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-04 20:46:44 +0200

    [UPDATE] Db_connect class to PDO

--- config/Db_connect.php
+++ config/Db_connect.php
@@ -1,5 +1,23 @@
 <?php
 class Db_connect
 {
-    
+    public function getConnecion()
+    {
+        $HOST = "localhost";
+        $USERNAME = "root";
+        $PASSWORD = "";
+        $DBNAME = "banque";
+        $DSN = "mysql:host=$HOST;dbname=$DBNAME;charset=utf8";
+        $opt = array(
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+            );
+        try{
+            $pdo = new PDO($DSN,$USERNAME,$PASSWORD,$opt);
+        }catch(PDOException $e){
+            die($e->__toString());
+        }
+        return $pdo;
+    }
 }
