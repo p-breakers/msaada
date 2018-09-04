2ff7d4aa07d4f28a2e4c89a4041183e20605b1b4
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-04 21:59:18 +0200

    [UPDATE] Copmte.php add getter and setter for num_client

--- controllers/Compte.php
+++ controllers/Compte.php
@@ -11,4 +11,20 @@ class Compte
     public $num_client;
     public $num_compte;
     public $solde;
+
+    /**
+     * @return mixed
+     */
+    public function getNumClient()
+    {
+        return $this->num_client;
+    }
+
+    /**
+     * @param mixed $num_client
+     */
+    public function setNumClient($num_client)
+    {
+        $this->num_client = $num_client;
+    }
 }
