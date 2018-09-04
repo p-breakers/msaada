e13488e831b662b9086df7e01a12afeb8a08fc7c
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-04 21:31:02 +0200

    [UPDATE] init add config array

--- config/init.php
+++ config/init.php
@@ -5,3 +5,10 @@
  * Date: 04/09/2018
  * Time: 20:49 PM
  */
+
+$Config["BASE_URL"] = 'http://'.$_SERVER["SERVER_NAME"]."/banque/";
+$Config["URL_SUFFIX"] = ".html";
+$Config["APP_NAME"] = "Banque";
+$Config["AUTHOR"];
+$Config["MAIL"];
+$Config["PHONE"];
