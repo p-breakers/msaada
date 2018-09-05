9a7f176174a4fe560abf6237e008b19b825e6282
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-05 14:46:51 +0200

    [UPDATE] class Banquier construct and doc

--- controllers/Banquier.php
+++ controllers/Banquier.php
@@ -1,5 +1,18 @@
 <?php
-class Banquier
+class Banquier extends Admin
 {
-    
+    public $type = "banquier";
+
+    /**
+     * Analyste constructor.
+     * @param $id
+     * @param $nom_complet
+     * @param $phone
+     * @param $motdepasse
+     * @param $mail
+     */
+    public function __construct($id, $nom_complet, $phone, $motdepasse, $mail)
+    {
+        parent::__construct($id, $nom_complet, $phone, $motdepasse, $mail, $this->type);
+    }
 }
