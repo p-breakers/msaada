d85a59a103c96b66fc3ec9f0bb287f010981af47
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-05 12:59:05 +0200

    [UPDATE] Client.php add attribute

--- controllers/Client.php
+++ controllers/Client.php
@@ -1,5 +1,13 @@
 <?php
 class Client
 {
-    
+    public $num_client;
+    public $nom_complet;
+    public $adresse;
+    public $nationalite;
+    public $genre;
+    public $phone;
+    public $email;
+    public $bday;
+    private static $motdepasse;
 }
