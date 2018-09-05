6ada927fa109c0cc5caa4eaeaa4b55249604a8a0
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-05 12:59:50 +0200

    [UPDATE] Client.php add getter and setter for num_client

--- controllers/Client.php
+++ controllers/Client.php
@@ -10,4 +10,20 @@ class Client
     public $email;
     public $bday;
     private static $motdepasse;
+
+    /**
+     * @return mixed
+     */
+    public function getNumClient()
+    {
+        return $this->num_client;
+    }
+
+    /**
+     * @param mixed $num_client
+     */
+    public function setNumClient($num_client): void
+    {
+        $this->num_client = $num_client;
+    }
 }
