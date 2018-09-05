19bae049b190d3fb508f137d6d5ea8aaf1ad6bec
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-05 14:44:34 +0200

    [UPDATE] class Analyste class make extends admin

--- controllers/Analyste.php
+++ controllers/Analyste.php
@@ -1,6 +1,14 @@
 <?php
 
-class Analyste
+class Analyste extends Admin
 {
-    
+
+    /**
+     * Analyste constructor.
+     */
+    public $type = "analyste";
+    public function __construct($id, $nom_complet, $phone, $motdepasse, $mail)
+    {
+        parent::__construct($id, $nom_complet, $phone, $motdepasse, $mail, $this->type);
+    }
 }
