10566a95b666867353ed42ed00a614e89e290217
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-05 14:45:43 +0200

    [UPDATE] class Analyste doc

--- controllers/Analyste.php
+++ controllers/Analyste.php
@@ -3,10 +3,16 @@
 class Analyste extends Admin
 {
 
+    public $type = "analyste";
+
     /**
      * Analyste constructor.
+     * @param $id
+     * @param $nom_complet
+     * @param $phone
+     * @param $motdepasse
+     * @param $mail
      */
-    public $type = "analyste";
     public function __construct($id, $nom_complet, $phone, $motdepasse, $mail)
     {
         parent::__construct($id, $nom_complet, $phone, $motdepasse, $mail, $this->type);
