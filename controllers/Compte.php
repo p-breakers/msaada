6760c1209f7c5ac8cee7a3717e929c4324f47897
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-05 12:30:31 +0200

    [UPDATE] add getter and setter for id

--- controllers/Compte.php
+++ controllers/Compte.php
@@ -78,4 +78,20 @@ class Compte
         $this->solde = $solde;
     }
 
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
 }
