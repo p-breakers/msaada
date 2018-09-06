ca761dcd5c221e0b453ca80f5ddb289224da37b4
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-06 22:54:05 +0200

    [UPDATE] ClientDAO add method addClient

--- models/ClientDAO.php
+++ models/ClientDAO.php
@@ -22,10 +22,43 @@ class ClientDAO extends Model
         $this->q->execute([
             "num_client" => $num_client
         ]);
-        return new Client($this->q->num_client, $this->q->nom_complet, $this->q->adresse, $this->q->nationalite, $this->q->genre, $this->q->phone, $this->q->email, $this->q->bday, $this->q->motdepasse);
+        return new Client($this->q->num_client, $this->q->nom_complet, $this->q->adresse, $this->q->nationalite,
+            $this->q->genre, $this->q->phone, $this->q->email, $this->q->bday, $this->q->motdepasse);
     }
 
-    public function addClient(){
-
+    /**
+     * @param $num_client
+     * @param $nom_complet
+     * @param $adresse
+     * @param $nationalite
+     * @param $genre
+     * @param $phone
+     * @param $email
+     * @param $bday
+     * @param $motdepasse
+     * @return bool|string
+     */
+    public function addClient($num_client, $nom_complet, $adresse, $nationalite, $genre, $phone, $email, $bday, $motdepasse){
+        try{
+            $this->q = $this->db->prepare("INSERT INTO 
+              clients(num_client, nom_complet, adresse, nationalite, genre, phone, email, bday, motdepasse) 
+              VALUES(:num_client, :nom_complet, :adresse, :nationalite, :genre, :phone, :email, :bday, :motdepasse)");
+            $this->q->execute([
+                "num_client" => $num_client,
+                "nom_complet" => $nom_complet,
+                "adresse" => $adresse,
+                "nationalite" => $nationalite,
+                "genre" => $genre,
+                "phone" => $phone,
+                "email" => $email,
+                "bday" => $bday,
+                "motdepasse" => $motdepasse
+            ]);
+            $d = true;
+        }catch (PDOException $e){
+            $d = $e->getMessage();
+        }finally{
+            return $d;
+        }
     }
 }
