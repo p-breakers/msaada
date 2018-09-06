aa91fea8e3b8223e3832e2904a91eede6a71788a
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-06 12:45:10 +0200

    [UPDATE] all add BASEPATH requirement

--- controllers/Analyste.php
+++ controllers/Analyste.php
@@ -1,5 +1,5 @@
 <?php
-
+defined('BASEPATH') OR exit('No direct script access allowed');
 class Analyste extends Admin
 {
 
--- controllers/Client.php
+++ controllers/Client.php
@@ -1,4 +1,5 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 class Client
 {
     public $num_client;
