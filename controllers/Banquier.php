9d73438c9179fbf24c5e572681e0ef776046dc45
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-07 12:06:03 +0200

    [UPDATE] Banquier add client

--- controllers/Banquier.php
+++ controllers/Banquier.php
@@ -16,4 +16,22 @@ class Banquier extends Admin
     {
         parent::__construct($id, $nom_complet, $phone, $motdepasse, $mail, $this->type);
     }
+
+    /**
+     * @param $num_client
+     * @param $nom_complet
+     * @param $adresse
+     * @param $nationalite
+     * @param $genre
+     * @param $phone
+     * @param $email
+     * @param $bday
+     * @param $motdepasse
+     * @return Client
+     */
+    public function addClient($num_client, $nom_complet, $adresse, $nationalite, $genre, $phone, $email, $bday, $motdepasse)
+    {
+        $motdepasse = $this->password_hash($motdepasse);
+        return new Client($num_client, $nom_complet, $adresse, $nationalite, $genre, $phone, $email, $bday, $motdepasse);
+    }
 }
