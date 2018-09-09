f90ac4456aad63cb974f82706ab7b561f657def0
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-09 22:22:01 +0200

    [UPDATE] add a static method to load a view

--- core/classes/load.php
+++ core/classes/load.php
@@ -8,5 +8,17 @@
 
 class load
 {
-
+    /**
+     * @param $viewFile
+     * @param array $viewVars
+     */
+    static function view($viewFile, $viewVars = array()){
+        extract($viewVars);
+        $viewFileCheck = explode(".", $viewFile);
+        if(!isset($viewFileCheck[1])){
+            $viewFile .= ".php";
+        }
+        $viewFile = str_replace("::", "/", $viewFile);
+        require_once $GLOBALS["config"]["path"]["app"]."views/{$viewFile}";
+    }
 }
