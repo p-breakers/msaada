80ca2c71ca5c5a08d9357a7a2a3e9e221f4e821a
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-09 12:46:37 +0200

    [UPDATE] index.php add some config

--- index.php
+++ index.php
@@ -1,4 +1,27 @@
 <?php
 error_reporting(E_ALL);
 ini_set("display_errors",1);
-include_once "config/init.php";
+$GLOBALS["config"] = array();
+$GLOBALS["config"]["BASE_URL"] = 'http://'.$_SERVER["SERVER_NAME"]."/banque/";
+$GLOBALS["config"]["URL_SUFFIX"] = ".html";
+$GLOBALS["config"]["APP_NAME"] = "Banque";
+$GLOBALS["config"]["AUTHOR"] = "";
+$GLOBALS["config"]["MAIL"] = "";
+$GLOBALS["config"]["PHONE"] = "";
+$GLOBALS["config"]["ACCOUNT_TYPE"] = ["ANALYSTE", "BANQUIER", "CLIENT"];
+$GLOBALS["config"]["DB"] = array();
+$GLOBALS["config"]["DB"]["HOST"] = "localhost";
+$GLOBALS["config"]["DB"]["USERNAME"] = "root";
+$GLOBALS["config"]["DB"]["PASSWORD"] = "";
+$GLOBALS["config"]["DB"]["DBNAME"] = "banque";
+$GLOBALS["config"]["PATH"] = [
+    "app" => "app/",
+    "core" => "core/",
+    "index" => "index.php"
+];
+$GLOBALS["config"]["DEFAULTS"] = [
+    "controller" => "main",
+    "method" => "index"
+];
+$GLOBALS["config"]["routes"] = [];
+include_once $GLOBALS["config"]["PATH"]['app']."config/init.php";
