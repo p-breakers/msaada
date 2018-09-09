ab1e8aff83d1f43dcdafa7efda0a87c08e51fa08
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-09 21:42:05 +0200

    [UPDATE] rearrange $GLOBALS['config']

--- index.php
+++ index.php
@@ -1,27 +1,24 @@
 <?php
 error_reporting(E_ALL);
 ini_set("display_errors",1);
-$GLOBALS["config"] = array();
-$GLOBALS["config"]["BASE_URL"] = 'http://'.$_SERVER["SERVER_NAME"]."/banque/";
-$GLOBALS["config"]["URL_SUFFIX"] = ".html";
-$GLOBALS["config"]["APP_NAME"] = "Banque";
-$GLOBALS["config"]["AUTHOR"] = "";
-$GLOBALS["config"]["MAIL"] = "";
-$GLOBALS["config"]["PHONE"] = "";
-$GLOBALS["config"]["ACCOUNT_TYPE"] = ["ANALYSTE", "BANQUIER", "CLIENT"];
-$GLOBALS["config"]["DB"] = array();
-$GLOBALS["config"]["DB"]["HOST"] = "localhost";
-$GLOBALS["config"]["DB"]["USERNAME"] = "root";
-$GLOBALS["config"]["DB"]["PASSWORD"] = "";
-$GLOBALS["config"]["DB"]["DBNAME"] = "banque";
-$GLOBALS["config"]["PATH"] = [
-    "app" => "app/",
-    "core" => "core/",
-    "index" => "index.php"
-];
-$GLOBALS["config"]["DEFAULTS"] = [
-    "controller" => "main",
-    "method" => "index"
-];
-$GLOBALS["config"]["routes"] = [];
-include_once $GLOBALS["config"]["PATH"]['core']."autoload.php";
+$GLOBALS["config"] = array(
+    "addName" => "Banque",
+    "domain" => "localhost",
+    "path" => array(
+        "app" => "app/",
+        "core" => "core/",
+        "index" => "index.php"
+    ),
+    "defaults" => array(
+        "controller" => "main",
+        "method" => "index"
+    ),
+    "routes" => array(),
+    "database" => array(
+        "host" => "localhost",
+        "username" => "root",
+        "password" => "",
+        "name" => "banque"
+    )
+);
+include_once $GLOBALS["config"]["path"]['core']."autoload.php";
