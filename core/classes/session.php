f842c7c260ede85665fbd920e6f028a0876205ba
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-10 23:04:59 +0200

    [UPDATE] add constructor

--- core/classes/session.php
+++ core/classes/session.php
@@ -8,5 +8,21 @@
 
 class session
 {
+    function __construct(){
+        if(!isset($_SESSION)){
+            session_start();
+        }
+        foreach ($_COOKIE as $key => $value) {
+            if(!isset($_SESSION[$key])){
+                json_decode($value);
+                if(json_last_error == JSON_ERROR_NONE){
+                    $_SESSION[$key] = json_decode($value);
+                } else {
+                    $_SESSION[$key] = $value;
+                }
+            }
+        }
+    }
+
 
 }
