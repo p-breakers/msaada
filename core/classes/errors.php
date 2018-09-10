6046756e1b90140c00d9f0785eb71fbb10c97888
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-10 22:41:59 +0200

    [NEW] model.php

--- core/classes/errors.php
+++ core/classes/errors.php
@@ -14,7 +14,7 @@ class errors
     static function show($type){
         switch ($type) {
             case 404:
-                echo "La page !<br>{$type} n'a pu &egrave;tre trouv&acute; ERREUR !";
+                echo "La page ! {$type} n'a pu &ecirc;tre trouv&eacute; ERREUR !";
                 break;
 
             default:
---
+++ core/classes/model.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ITOT
+ * Date: 10/09/2018
+ * Time: 22:41 PM
+ */
+
+class model
+{
+    public $model;
+}
