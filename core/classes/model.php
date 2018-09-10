ff2cebb79058417a8e59fc3890807857f81845b7
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-10 22:46:01 +0200

    [UPDATE] model.php add constructor

--- core/classes/model.php
+++ core/classes/model.php
@@ -9,4 +9,10 @@
 class model
 {
     public $model;
+    function __construct(){
+        $this->model = new database($GLOBALS["config"]["database"]["host"],
+            $GLOBALS["config"]["database"]["username"],
+            $GLOBALS["config"]["database"]["password"],
+            $GLOBALS["config"]["database"]["database"]);
+    }
 }
