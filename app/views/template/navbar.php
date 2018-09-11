2620767b7882884ae7847ca18f8a6ae3879a382c
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-11 23:23:31 +0200

    [UPDATE] navbar for part selection

--- app/views/template/navbar.php
+++ app/views/template/navbar.php
@@ -1,7 +1,10 @@
 <!-- /w3_agileits_top_nav-->
 <!-- /nav-->
 <div class="w3_agileits_top_nav">
-
+    <ul id="gn-menu" class="gn-menu-main">
+        <?php load::view("template::sidebar") ?>
+        <li class="second logo"><h1><a href="main-page.html"><i class="fa fa-graduation-cap" aria-hidden="true"></i><?= $GLOBALS["config"]["appName"] ?></a></h1></li>
+            <?php load::view("template::navcontent") ?>
         <li class="second full-screen">
             <section class="full-top">
                 <button id="toggle"><i class="fa fa-arrows-alt" aria-hidden="true"></i></button>
