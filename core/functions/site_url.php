a00fedcc4a17c1561936fcf0bf1f3c7fcf48a2d6
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-12 00:03:39 +0200

    [UPDATE] change extension to url_suffix

--- core/functions/site_url.php
+++ core/functions/site_url.php
@@ -6,7 +6,7 @@
 function site_url(string $url)
 {
     if (isset($GLOBALS["config"]['extension']))
-        return "http://".$GLOBALS['config']['domain']."/".$url.".".$GLOBALS["config"]['extension'];
+        return "http://".$GLOBALS['config']['domain']."/".$url.".".$GLOBALS["config"]['url_suffix'];
     else
         return "http://".$GLOBALS['config']['domain']."/".$url.".php";
 }
