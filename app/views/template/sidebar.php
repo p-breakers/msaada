ac42126f3f15e9ef3340b3338d7f7cc161a56710
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-11 23:11:04 +0200

    [NEW] navcontent

--- app/views/template/sidebar.php
+++ app/views/template/sidebar.php
@@ -1,7 +1,55 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ITOT
- * Date: 11/09/2018
- * Time: 23:06 PM
- */
+<!-- /nav_agile_w3l -->
+<li class="gn-trigger">
+    <a class="gn-icon gn-icon-menu"><i class="fa fa-bars" aria-hidden="true"></i><span>Menu</span></a>
+    <nav class="gn-menu-wrapper">
+        <div class="gn-scroller scrollbar1">
+            <ul class="gn-menu agile_menu_drop">
+                <li><a href="main-page.html"> <i class="fa fa-tachometer"></i> Dashboard</a></li>
+                <li>
+                    <a href="#"><i class="fa fa-cogs" aria-hidden="true"></i> UI Components <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                    <ul class="gn-submenu">
+                        <li class="mini_list_agile"><a href="buttons.html"><i class="fa fa-caret-right" aria-hidden="true"></i> Buttons</a></li>
+                        <li class="mini_list_w3"><a href="grids.html"> <i class="fa fa-caret-right" aria-hidden="true"></i> Grids</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#"> <i class="fa fa-file-text-o" aria-hidden="true"></i>Forms <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                    <ul class="gn-submenu">
+                        <li class="mini_list_agile"><a href="input.html"><i class="fa fa-caret-right" aria-hidden="true"></i> Inputs</a></li>
+                        <li class="mini_list_w3"><a href="validation.html"><i class="fa fa-caret-right" aria-hidden="true"></i> Validation</a></li>
+                    </ul>
+                </li>
+                <li><a href="table.html"> <i class="fa fa-table" aria-hidden="true"></i> Tables</a></li>
+                <li><a href="#"><i class="fa fa-list" aria-hidden="true"></i>Short Codes <i class="fa fa-angle-down" aria-hidden="true"> </i></a>
+                    <ul class="gn-submenu">
+                        <li class="mini_list_agile"><a href="typo.html"><i class="fa fa-caret-right" aria-hidden="true"></i> Typography</a></li>
+                        <li class="mini_list_w3"><a href="icons.html"> <i class="fa fa-caret-right" aria-hidden="true"></i> Icons</a></li>
+
+                    </ul>
+                </li>
+
+                <li><a href="charts.html"> <i class="fa fa-line-chart" aria-hidden="true"></i> Charts</a></li>
+                <li><a href="maps.html"><i class="fa fa-map-o" aria-hidden="true"></i> Maps</a></li>
+                <li class="page">
+                    <a href="#"><i class="fa fa-files-o" aria-hidden="true"></i> Pages <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                    <ul class="gn-submenu">
+
+                        <li class="mini_list_agile"> <a href="signin.html"> <i class="fa fa-caret-right" aria-hidden="true"></i> Sign In</a></li>
+                        <li class="mini_list_w3"><a href="signup.html"> <i class="fa fa-caret-right" aria-hidden="true"></i> Sign Up</a></li>
+                        <li class="mini_list_agile error"><a href="404.html"> <i class="fa fa-caret-right" aria-hidden="true"></i> Error 404 </a></li>
+
+                        <li class="mini_list_w3_line"><a href="calendar.html"> <i class="fa fa-caret-right" aria-hidden="true"></i> Calendar</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#"> <i class="fa fa-suitcase" aria-hidden="true"></i>More <i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                    <ul class="gn-submenu">
+                        <li class="mini_list_agile"><a href="faq.html"> <i class="fa fa-caret-right" aria-hidden="true"></i> Faq</a></li>
+                        <li class="mini_list_w3"><a href="blank.html"> <i class="fa fa-caret-right" aria-hidden="true"></i> Blank Page</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div><!-- /gn-scroller -->
+    </nav>
+</li>
+<!-- //nav_agile_w3l -->
