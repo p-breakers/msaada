2af0ea32fd54abb8ec59fc8a7b556d13bb6a80f1
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-11 22:24:20 +0200

    [UPDATE] modify foot js and others

--- app/views/template/foot.php
+++ app/views/template/foot.php
@@ -1,3 +1,114 @@
-        </div>
-    </body>
+<!--copy rights start here-->
+<div class="copyrights">
+    <p>© <?= date("Y"); ?> Eunice Bank. Tous droits r&eacute;serv&eacute; | Design by  <a href="http://localhost/" target="_blank">Eunice</a> </p>
+</div>
+<!--copy rights end here-->
+<!-- js -->
+
+<script type="text/javascript" src="assets/js/jquery-2.1.4.min.js"></script>
+<script src="assets/js/modernizr.custom.js"></script>
+
+<script src="assets/js/classie.js"></script>
+<script src="assets/js/gnmenu.js"></script>
+<script>
+    new gnMenu( document.getElementById( 'gn-menu' ) );
+</script>
+<script type="text/javascript" src="assets/https://maps.googleapis.com/maps/api/js"></script>
+<script type="text/javascript">
+    google.maps.event.addDomListener(window, 'load', init);
+    function init() {
+        var mapOptions = {
+            zoom: 11,
+            center: new google.maps.LatLng(40.6700, -73.9400),
+            styles: [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
+        };
+        var mapElement = document.getElementById('map');
+        var map = new google.maps.Map(mapElement, mapOptions);
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(40.6700, -73.9400),
+            map: map,
+        });
+    }
+</script>
+<script src="assets/js/prettymaps.js"></script>
+<script>
+
+    $(function(){
+        //default
+        $('.map-canvas').prettyMaps({
+            address: 'Melbourne, Australia',
+            image: 'map-icon.png',
+            hue: '#FF0000',
+            saturation: -20
+        });
+
+        //red map example
+        $('#default-map-btn').on('click', function(){
+            $('.map-canvas').prettyMaps();
+        });
+
+        //green map example
+        $('#green-map-btn').on('click', function(){
+            $('.map-canvas').prettyMaps({
+                address: 'Melbourne, Australia',
+                image: 'map-icon.png',
+                hue: '#00FF55',
+                saturation: -30
+            });
+        });
+
+        //blue map example
+        $('#blue-map-btn').on('click', function(){
+            $('.map-canvas').prettyMaps({
+                address: 'Melbourne, Australia',
+                image: 'map-icon.png',
+                hue: '#0073FF',
+                saturation: -30,
+                zoom: 16,
+                panControl: true,
+                zoomControl: true,
+                mapTypeControl: true,
+                scaleControl: true,
+                streetViewControl: true,
+                overviewMapControl: true,
+                scrollwheel: false,
+            });
+        });
+
+        //grey map example
+        $('#grey-map-btn').on('click', function(){
+            $('.map-canvas').prettyMaps({
+                address: 'Melbourne, Australia',
+                image: 'map-icon.png',
+                saturation: -100,
+                lightness: 10
+            });
+        });
+    });
+
+</script>
+<!-- //js -->
+<script src="assets/js/screenfull.js"></script>
+<script>
+    $(function () {
+        $('#supported').text('Supported/allowed: ' + !!screenfull.enabled);
+
+        if (!screenfull.enabled) {
+            return false;
+        }
+
+
+
+        $('#toggle').click(function () {
+            screenfull.toggle($('#container')[0]);
+        });
+    });
+</script>
+<script src="assets/js/jquery.nicescroll.js"></script>
+<script src="assets/js/scripts.js"></script>
+
+<script type="text/javascript" src="assets/js/bootstrap-3.1.1.min.js"></script>
+
+
+</body>
 </html>
