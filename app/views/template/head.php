a3d3b4e606eb9c6820bf2e9948866a4ed82eeda0
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-12 00:06:05 +0200

    [UPDATE] add keyword and author meta

--- app/views/template/head.php
+++ app/views/template/head.php
@@ -5,7 +5,8 @@
     <!-- custom-theme -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="keywords" content="" />
+    <meta name="keywords" content="<?= $GLOBALS['config']['appName'] ?>, <?= $GLOBALS['config']['author'] ?>" />
+    <meta name="author" content="<?= $GLOBALS['config']['author'] ?>">
     <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
         function hideURLbar(){ window.scrollTo(0,1); } </script>
     <!-- //custom-theme -->
