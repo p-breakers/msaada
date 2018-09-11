d4dc5216dec5a6701b5d836b70d825c6b6aa2f4d
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-11 22:03:54 +0200

    [UPDATE] add func method

--- core/classes/load.php
+++ core/classes/load.php
@@ -12,7 +12,8 @@ class load
      * @param string $viewFile
      * @param array $viewVars
      */
-    static function view(string $viewFile, array  $viewVars = array()){
+    static function view(string $viewFile, array  $viewVars = array())
+    {
         extract($viewVars);
         $viewFileCheck = explode(".", $viewFile);
         if(!isset($viewFileCheck[1])){
@@ -20,5 +21,10 @@ class load
         }
         $viewFile = str_replace("::", "/", $viewFile);
         require_once $GLOBALS["config"]["path"]["app"]."views/{$viewFile}";
+    }
+
+    static function func()
+    {
+
     }
 }
