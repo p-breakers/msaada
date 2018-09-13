f791576a178875d8cb877fce8e0a528eaf187982
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-14 01:09:52 +0200

    [UPDATE] verify session variable

--- app/views/template/navbar.php
+++ app/views/template/navbar.php
@@ -2,9 +2,11 @@
 <!-- /nav-->
 <div class="w3_agileits_top_nav">
     <ul id="gn-menu" class="gn-menu-main">
-        <?php load::view("template::sidebar") ?>
-        <li class="second logo"><h1><a href="main-page.html"><i class="fa fa-graduation-cap" aria-hidden="true"></i><?= $GLOBALS["config"]["appName"] ?></a></h1></li>
-            <?php load::view("template::navcontent") ?>
+        <?php if (isset($_SESSION['user'])) load::view("template::sidebar"); ?>
+        <li class="second logo"><h1><a href="main-page.html"><i class="fa fa-graduation-cap"
+                                                                aria-hidden="true"></i><?= $GLOBALS["config"]["appName"] ?>
+                </a></h1></li>
+        <?php if (isset($_SESSION['user'])) load::view("template::navcontent"); ?>
         <li class="second full-screen">
             <section class="full-top">
                 <button id="toggle"><i class="fa fa-arrows-alt" aria-hidden="true"></i></button>
