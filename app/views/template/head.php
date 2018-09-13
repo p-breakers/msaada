c02e431d979639511f8c84bd6ee721507c48737d
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-14 00:37:33 +0200

    [UPDATE] add site::base_url to assets component importation

--- app/views/template/head.php
+++ app/views/template/head.php
@@ -3,25 +3,26 @@
 <head>
     <title>Eunice Banque | <?= isset($title) ? $title : "La banque du moment"; ?></title>
     <!-- custom-theme -->
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="keywords" content="<?= $GLOBALS['config']['appName'] ?>, <?= $GLOBALS['config']['author'] ?>" />
     <meta name="author" content="<?= $GLOBALS['config']['author'] ?>">
     <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
         function hideURLbar(){ window.scrollTo(0,1); } </script>
     <!-- //custom-theme -->
-    <link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
-    <link href="assets/css/component.css" rel="stylesheet" type="text/css" media="all" />
-    <link href="assets/css/export.css" rel="stylesheet" type="text/css" media="all" />
-    <link href="assets/css/flipclock.css" rel="stylesheet" type="text/css" media="all" />
-    <link href="assets/css/circles.css" rel="stylesheet" type="text/css" media="all" />
-    <link href="assets/css/style_grid.css" rel="stylesheet" type="text/css" media="all" />
-    <link href="assets/css/style.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="<?= site::base_url("assets/css/bootstrap.css")?>" rel="stylesheet" type="text/css" media="all" />
+    <link href="<?= site::base_url("assets/css/component.css") ?>" rel="stylesheet" type="text/css" media="all" />
+    <link href="<?= site::base_url("assets/css/export.css") ?>" rel="stylesheet" type="text/css" media="all" />
+    <link href="<?= site::base_url("assets/css/flipclock.css") ?>" rel="stylesheet" type="text/css" media="all" />
+    <link href="<?= site::base_url("assets/css/circles.css") ?>" rel="stylesheet" type="text/css" media="all" />
+    <link href="<?= site::base_url("assets/css/style_grid.css") ?>" rel="stylesheet" type="text/css" media="all" />
+    <link href="<?= site::base_url("assets/css/style.css") ?>" rel="stylesheet" type="text/css" media="all" />
 
     <!-- font-awesome-icons -->
-    <link href="assets/css/font-awesome.css" rel="stylesheet">
+    <link href="<?= site::base_url("assets/css/font-awesome.css") ?>" rel="stylesheet"/>
     <!-- //font-awesome-icons -->
-    <link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800" rel="stylesheet">
+    <link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800" rel="stylesheet"/>
+    <link rel="shortcut icon" href="<?= site::base_url("favicon.ico") ?>" type="image/x-icon"/>
 
 </head>
 <body>
