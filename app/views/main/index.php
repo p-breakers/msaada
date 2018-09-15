101c17cb843960e4d1b3a55cfe23d00959213c09
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-15 19:28:04 +0200

    [UPDATE] add some content

--- app/views/main/index.php
+++ app/views/main/index.php
@@ -1,12 +1,12 @@
 <?php load::view("template::head")?>
 <?php load::view("template::navbar")?>
-<div class="inner_content">
-    <div class="starter-template">
-        <h1>Bootstrap starter template</h1>
-        <p class="lead">
-            Use this document as a way to quickly start any new project.<br>
-            All you get is this text and a mostly barebones HTML document.
-        </p>
+    <div class="inner_content">
+        <div class="starter-template">
+            <h1>Bootstrap starter template</h1>
+            <p class="lead">
+                Use this document as a way to quickly start any new project.<br>
+                All you get is this text and a mostly barebones HTML document.
+            </p>
+        </div>
     </div>
-</div>
 <?php load::view("template::foot")?>
