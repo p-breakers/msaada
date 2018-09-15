7e57806ca02c8c949b1eea3c1a078ffc64bd0201
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-15 19:26:47 +0200

    [UPDATE] change session check

--- app/views/template/navbar.php
+++ app/views/template/navbar.php
@@ -2,11 +2,11 @@
 <!-- /nav-->
 <div class="w3_agileits_top_nav">
     <ul id="gn-menu" class="gn-menu-main">
-        <?php if (session::get('user')) load::view("template::sidebar"); ?>
+        <?php if (session::check('id_admin')) load::view("template::sidebar"); ?>
         <li class="second logo"><h1><a href="<?= site::base_url() ?>"><i class="fa fa-graduation-cap"
                                                                          aria-hidden="true"></i><?= $GLOBALS["config"]["appName"] ?>
                 </a></h1></li>
-        <?php if (session::get('user')) load::view("template::navcontent"); ?>
+        <?php if (session::check('id_admin')) load::view("template::navcontent"); ?>
         <li class="second full-screen">
             <section class="full-top">
                 <button id="toggle"><i class="fa fa-arrows-alt" aria-hidden="true"></i></button>
