ead489d050517b02b8902b787414daa42092ec93
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-15 22:10:39 +0200

    [UPDATE] add some content

--- app/views/main/banquier.php
+++ app/views/main/banquier.php
@@ -1,12 +1,67 @@
 <?php load::view("template::head") ?>
 <?php load::view("template::navbar") ?>
     <div class="inner_content">
-        <div class="starter-template">
-            <h1>BANQUIER</h1>
-            <p class="lead">
-                Use this document as a way to quickly start any new project.<br>
-                All you get is this text and a mostly barebones HTML document.
-            </p>
+        <div class="inner_content_w3_agile_info">
+            <!-- /agile_top_w3_grids-->
+            <div class="agile_top_w3_grids">
+                <ul class="ca-menu">
+                    <li>
+                        <a href="#">
+
+                            <i class="fa fa-building-o" aria-hidden="true"></i>
+                            <div class="ca-content">
+                                <h4 class="ca-main">16,808</h4>
+                                <h3 class="ca-sub">New Companies</h3>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-user" aria-hidden="true"></i>
+                            <div class="ca-content">
+                                <h4 class="ca-main one">26,808</h4>
+                                <h3 class="ca-sub one">New Clients</h3>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-database" aria-hidden="true"></i>
+                            <div class="ca-content">
+                                <h4 class="ca-main two">29,008</h4>
+                                <h3 class="ca-sub two">New Projects</h3>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-tasks" aria-hidden="true"></i>
+                            <div class="ca-content">
+                                <h4 class="ca-main three">49,436</h4>
+                                <h3 class="ca-sub three">Old Projects</h3>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="fa fa-clone" aria-hidden="true"></i>
+                            <div class="ca-content">
+                                <h4 class="ca-main four">30,808</h4>
+                                <h3 class="ca-sub four">New Orders</h3>
+                            </div>
+                        </a>
+                    </li>
+                    <div class="clearfix"></div>
+                </ul>
+            </div>
+            <!-- //agile_top_w3_grids-->
+            <div class="starter-template">
+                <h1>BANQUIER</h1>
+                <p class="lead">
+                    Use this document as a way to quickly start any new project.<br>
+                    All you get is this text and a mostly barebones HTML document.
+                </p>
+            </div>
         </div>
     </div>
 <?php load::view("template::foot") ?>
