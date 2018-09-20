5cdcc2fd62fd44c86b325205dc22d8167ed256b8
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-20 12:46:54 +0200

    [UPDATE] add facteur on identif_risq method

--- app/models/ClientDAO.php
+++ app/models/ClientDAO.php
@@ -214,16 +214,17 @@ class ClientDAO extends Model2
         }
     }
 
-    public function identif_risq($cap_perm, $val_imm, $cap_credit, $actif)
+    public function identif_risq($cap_perm, $val_imm, $cap_credit, $actif, $facteur)
     {
         try {
             $ratio_solv = $actif / $cap_credit;
             $ratio_fin = $cap_perm / $val_imm;
             $this->q = $this->db->prepare("UPDATE demande SET 
           actif = :actif, ratio_financement = :ratio_fin, ratio_solvabilite = :ratio_solv, valeurs_immobilisees = :val_imm,
-          capitaux_permanent = :cap_perm, capitaux_credit = :cap_credit");
-            $this->q->execute(["actif" => $actif, "ratio_fin" => $ratio_fin, "ratio_solv" => $ratio_solv, "cap_credit" => $cap_credit, "cap_perm" => $cap_perm, "val_imm" => $val_imm]);
+          capitaux_permanent = :cap_perm, capitaux_credit = :cap_credit, facteur = :facteur");
+            $this->q->execute(["actif" => $actif, "ratio_fin" => $ratio_fin, "ratio_solv" => $ratio_solv, "cap_credit" => $cap_credit, "cap_perm" => $cap_perm, "val_imm" => $val_imm, "facteur" => $facteur]);
             $this->d = true;
+
         } catch (PDOException $e) {
             $this->d = $e->__toString();
         } finally {
