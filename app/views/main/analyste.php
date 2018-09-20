2b4458b2ea2ee691e0f6f70461c0473c02ae65d6
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-20 10:41:30 +0200

    [UPDATE] add list views on analyste

--- app/views/main/analyste.php
+++ app/views/main/analyste.php
@@ -5,48 +5,57 @@
             <!-- /agile_top_w3_grids-->
             <div class="agile_top_w3_grids">
                 <ul class="ca-menu">
-                    <li>
-                        <a href="#">
+                    <!--<li>
+                        <a href="<? /*= site::site_url("analyst/add_client") */ ?>">
                             <i class="fa fa-building-o" aria-hidden="true"></i>
                             <div class="ca-content">
-                                <h4 class="ca-main">16,808</h4>
-                                <h3 class="ca-sub">New Companies</h3>
+                                <h4 class="ca-main"><? /*= $nbClient */ ?></h4>
+                                <h3 class="ca-sub">Ajouter un client</h3>
                             </div>
                         </a>
                     </li>
                     <li>
-                        <a href="#">
+                        <a href="<? /*= site::site_url("analyst/add_demande") */ ?>">
                             <i class="fa fa-user" aria-hidden="true"></i>
                             <div class="ca-content">
-                                <h4 class="ca-main one">26,808</h4>
-                                <h3 class="ca-sub one">New Clients</h3>
+                                <h4 class="ca-main one"><? /*= $nbDemande */ ?></h4>
+                                <h3 class="ca-sub one">Ajouter une demande</h3>
                             </div>
                         </a>
-                    </li>
+                    </li>-->
                     <li>
-                        <a href="#">
+                        <a href="<?= site::site_url("analyst/list_clients") ?>">
                             <i class="fa fa-database" aria-hidden="true"></i>
                             <div class="ca-content">
-                                <h4 class="ca-main two">29,008</h4>
-                                <h3 class="ca-sub two">New Projects</h3>
+                                <h4 class="ca-main two"><?= $nbClient ?></h4>
+                                <h3 class="ca-sub two">Liste des clients</h3>
                             </div>
                         </a>
                     </li>
                     <li>
-                        <a href="#">
-                            <i class="fa fa-tasks" aria-hidden="true"></i>
-                            <div class="ca-content">
-                                <h4 class="ca-main three">49,436</h4>
-                                <h3 class="ca-sub three">Old Projects</h3>
-                            </div>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
+                        <a href="<?= site::site_url("analyst/list_demandes") ?>">
                             <i class="fa fa-clone" aria-hidden="true"></i>
                             <div class="ca-content">
-                                <h4 class="ca-main four">30,808</h4>
-                                <h3 class="ca-sub four">New Orders</h3>
+                                <h4 class="ca-main four"><?= $nbDemande ?></h4>
+                                <h3 class="ca-sub four">Liste des demandes</h3>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?= site::site_url("analyst/list_comptes") ?>">
+                            <i class="fa fa-tasks" aria-hidden="true"></i>
+                            <div class="ca-content">
+                                <h4 class="ca-main three"><?= $nbCompte ?></h4>
+                                <h3 class="ca-sub three">Liste des comptes</h3>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="<?= site::site_url("analyst/list_credits") ?>">
+                            <i class="fa fa-clone" aria-hidden="true"></i>
+                            <div class="ca-content">
+                                <h4 class="ca-main four"><?= $nbCredit ?></h4>
+                                <h3 class="ca-sub four">Liste des cr&eacute;dits</h3>
                             </div>
                         </a>
                     </li>
