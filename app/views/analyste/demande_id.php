50da09b08450a877d5edba1d07981bf800de0301
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-25 19:55:05 +0200

    [UPDATE] add view for demande

--- app/views/analyste/demande_id.php
+++ app/views/analyste/demande_id.php
@@ -2,7 +2,6 @@
 <?php load::view("template::navbar") ?>
     <div class="inner_content">
         <!-- /pages_agile_info_w3l -->
-
         <div class="pages_agile_info_w3l">
             <!-- /login -->
             <div class="over_lay_agile_pages_w3ls one">
@@ -12,15 +11,28 @@
                         <div class="login-form form-body form-body-info">
                             <form action="<?= site::site_url("bankier/add_client") ?>" method="post"
                                   accept-charset="UTF-8">
-                                <?php
-                                foreach ($cl as $key => $value):
-                                    ?>
-                                    <label for="<?= $key ?>"><?= ucwords(str_replace("_", " ", $key)) ?></label>
-                                    <input id="<?= $key ?>" type="<?= ($value === 0) ? "number" : "text" ?>"
-                                           name="<?= $key ?>" value="<?= $value ?>" required=""/>
-                                <?php
-                                endforeach;
-                                ?>
+                                <input type="hidden" name="id" value="<?= cl['id'] ?>"/>
+                                <label for="cap_perm" class="control-label">Capitaux Permanent</label>
+                                <input type="number" value="<?= $cl['capitaux_permanent'] ?>" class="form-control"
+                                       id="cap_perm" name="cap_perm" required/>
+                                <label for="val_imm" class="control-label">Valeur Immobili&egrave;re</label>
+                                <input type="number" value="<?= $cl['valeurs_immobilisees'] ?>" class="form-control"
+                                       id="val_imm" name="val_imm" required/>
+                                <label for="actif" class="control-label">Actif</label>
+                                <input type="number" value="<?= $cl['acticf'] ?>" class="form-control" id="actif"
+                                       name="actif" required/>
+                                <label for="cap_cr" class="control-label">Capitaux Credit</label>
+                                <input type="number" value="<?= $cl['capitaux_credit'] ?>" class="form-control"
+                                       id="cap_cr" name="cap_cr" required/>
+                                <label for="facteur" class="control-label">Facteur</label>
+                                <select name="facteur" class="form-control" id="facteur" required>
+                                    <option value="taux interet" <?= ($cl['facteur'] == 'taux interet') ? "selected" : "" ?>>
+                                        Taux d'intérêt
+                                    </option>
+                                    <option value="cours de change" <?= ($cl['facteur'] == 'cours de change') ? "selected" : "" ?>>
+                                        Cours de change
+                                    </option>
+                                </select>
                                 <div class="tp">
                                     <input type="submit" value="Enregistrer">
                                 </div>
