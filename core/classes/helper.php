ed7f9cc293ef10578bca48297cbc677ad522b5d9
Jonathan YOMBO <jonathanyombo@gmail.com>
2018-09-25 19:48:43 +0200

    [ADD] add randomString core class method

--- core/classes/helper.php
+++ core/classes/helper.php
@@ -16,4 +16,20 @@ class helper
     {
         return htmlspecialchars($string, ENT_QUOTES, 'UTF-8', TRUE);
     }
+
+    /**
+     * @param int $length
+     * @return string
+     */
+    static function randomString(int $length = 12)
+    {
+        $str = "";
+        $characters = array_merge(range('A', 'Z'), range('a', 'z'), range('0', '9'));
+        $max = count($characters) - 1;
+        for ($i = 0; $i < $length; $i++) {
+            $rand = mt_rand(0, $max);
+            $str .= $characters[$rand];
+        }
+        return $str;
+    }
 }
